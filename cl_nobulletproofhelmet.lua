5665b7df51a8fad406365bda6cd3f71d852b8209
Wolfhill09 <harvey.akhurst@icloud.com>
2022-02-08 21:48:39 +0000

    Add read me

--- cl_nobulletproofhelmet.lua
+++ cl_nobulletproofhelmet.lua
@@ -3,6 +3,5 @@ CreateThread(function()
         Wait(0)
         SetPlayerLockon(PlayerId(), false)
         SetPedConfigFlag(PlayerPedId(), 149, true)
-        SetPedConfigFlag(PlayerPedId(), 438, true)
     end
 end)
